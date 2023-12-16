195188df087c9ffeafcbbcfe1f342e17148d75dc
sijia.wang <sijia.wang@funplus.com>
2023-12-16 16:59:23 +0800

    start : redo UI

---
+++ Assets/Scripts/UICharacterHealth.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using CreatorKitCode;
+
+[RequireComponent(typeof(Slider))]
+public class UICharacterHealth : MonoBehaviour
+{
+	public CharacterData data;
+
+	Slider slider;
+	private void Start()
+	{
+		slider = GetComponent<Slider>();
+	}
+	private void Update()
+	{
+		if (data && slider)
+		{
+			slider.value = data.Stats.CurrentHealth / (float)data.Stats.stats.health;
+			// MaxHealth.text = PlayerCharacter.Data.Stats.stats.health.ToString();
+			// CurrentHealth.text = PlayerCharacter.Data.Stats.CurrentHealth.ToString();
+		}
+	}
+}
